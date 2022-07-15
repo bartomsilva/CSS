cd5933a888f6f8eded58d4b862fa47887be26052
bartomsilva <bartomsilva@gmail.com>
2022-07-15 20:10:46 -0300

    adicao de novo estudo ajusta javascript

--- displayinline.js
+++ displayinline.js
@@ -1,25 +1,29 @@
-const $elementExample_first = document.querySelector("#estudo_um");
-const $elementExample_second = document.querySelector("#estudo_dois");
-const $elementExample_third = document.querySelector("#estudo_tres");
+//const pedro = document.getElementsByTagName("li");   
+//const pedro = document.getElementsByClassName("example-item");
+const pedro = document.querySelectorAll(".example li");    
+
+// "uma flag" (das antigas anos 80) serve marcar, chavear
+// tipo um liga desliga.
 let $Aplicado = false;
 
+//esconde o conteúdo explicativo 
 document.getElementById("estudo_div").style.display = 'none';
 
-function aplicarPropriedade() {        
-    if ($Aplicado) {        
-        $elementExample_first.classList.toggle('aplicar-propriedade');        
-        $elementExample_second.classList.toggle('aplicar-propriedade');                
-        $elementExample_third.classList.toggle('aplicar-propriedade');        
-        $Aplicado=false;
+function aplicarPropriedade() {
+    // aplica ou remove a propeidade display: inline 
+    // mostra ou esconde o texto explicativo     
+    if ($Aplicado) {    
+        for ( let i=0; i< pedro.length; i++){
+             pedro[i].classList.toggle('aplicar-propriedade');        
+         }
+         $Aplicado=false;
         document.getElementById("estudo_div").style.display = 'none';
-
-    } else {
-        $elementExample_first.classList.add('aplicar-propriedade');
-        $elementExample_second.classList.add('aplicar-propriedade');
-        $elementExample_third.classList.add('aplicar-propriedade');
+     } else {
+        for ( let i=0; i< pedro.length; i++){
+             pedro[i].classList.add('aplicar-propriedade');
+        }
         $Aplicado=true;        
         document.getElementById("estudo_div").style.display = 'block';
-
     }
 }
 
